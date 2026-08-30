73ef986a06dba369651407657db4135b4c3c2784
buolotdev <buolotdev@example.com>
2026-08-31 02:53:13 +0500

    fix: restore missing get import in technician profile settings

--- lib/technician_profile_settings_screen.dart
+++ lib/technician_profile_settings_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'app_state.dart';
